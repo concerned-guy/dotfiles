e03732a141e4334db7a83c5f56e55f5edf388ed0
s <vuongbaoson.1998@gmail.com>
2018-07-24 09:06:18 +0700

    another cleanup

--- bashrc
+++
@@ -1,4 +0,0 @@
-# Minimalist
-alias e='emacsclient -t --alternate-editor='
-alias ls='ls --color'
-export EDITOR='emacsclient -t --alternate-editor='
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,14 @@
-;;; init.el --- Emacs initialization file
+;;; init.el --- emacs initialization file
 
-;; Load Emacs Lisp packages and activate them
+;; Load emacs lisp packages and activate them
 (package-initialize)
 
 ;; Global behavior
 (setq-default tab-width 4           ;tab equals 4 spaces
-              indent-tabs-mode nil  ;spaces instead of tabs
-              vc-follow-symlinks t) ;follow links in link farms
+	          c-basic-offset 4      ;C language indentation
+	          indent-tabs-mode nil  ;spaces instead of tabs
+	          vc-follow-symlinks t) ;follow links in link farms
+
 ;; Store backups in ~/.emacs.d
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))
       backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
@@ -16,13 +18,6 @@
 (electric-pair-mode)
 (global-font-lock-mode -1)
 (menu-bar-mode -1)
-(recentf-mode)
 
-;; Keybindings for comfortable editing experience
-(global-set-key (kbd "C-c f") 'recentf-open-files)
+;; Keybindings
 (global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "<f5>") 'smart-compile)
-
-;; Package-specific settings
-(setq c-default-style "stroustrup") ;c-mode
-
