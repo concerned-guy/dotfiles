beae5490d7830d9db85bb102217c7e2f8a164cde
s <vuongbaoson.1998@gmail.com>
2018-07-31 14:33:01 +0700

    refactor

---
+++ emacs.el
@@ -0,0 +1,14 @@
+;;; init.el --- emacs initialization file
+
+;; Global behavior
+(setq-default tab-width 4               ;tab equals 4 spaces
+              c-basic-offset 4          ;C language indentation
+              indent-tabs-mode nil      ;spaces instead of tabs
+              auto-save-default nil     ;disable autosaves
+              make-backup-files nil     ;disable backups
+              vc-follow-symlinks t)     ;follow links in link farms
+
+;; Minor modes
+(global-font-lock-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
