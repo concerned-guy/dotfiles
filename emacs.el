c4644359c57dbc9a993a0bceb81a66e455568045
s <vuongbaoson.1998@gmail.com>
2018-08-09 22:49:21 +0700

    emacs

--- emacs.el
+++ emacs.el
@@ -3,9 +3,11 @@
 ;; Global behavior
 (setq-default tab-width 4               ;tab equals 4 spaces
               c-basic-offset 4          ;C language indentation
-              indent-tabs-mode nil      ;spaces instead of tabs
-              auto-save-default nil     ;disable autosaves
-              make-backup-files nil)    ;disable backups
+              indent-tabs-mode nil)     ;spaces instead of tabs
+;; Less start time with these...
+(setq auto-save-default nil             ;disable autosaves
+      make-backup-files nil             ;disable backups
+      auto-save-list-file-prefix nil)   ;disable listing of autosaves
 
 ;; Minor modes
 (global-font-lock-mode -1)
