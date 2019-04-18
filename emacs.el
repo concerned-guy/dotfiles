35ab5a6d263369501f0ef876bd7a6e6ac319f343
concerned-guy <34766392+concerned-guy@users.noreply.github.com>
2019-04-18 18:21:48 +0700

    s

--- emacs.el
+++ emacs.el
@@ -3,7 +3,7 @@
 ;; Global behavior
 (setq auto-save-default nil)            ;disable autosaves
 (setq auto-save-list-file-prefix nil)   ;disable listing of autosaves
-(setq inhibit-startup-screen t)         ;disable startup screen
+(setq inhibit-startup-message t)        ;disable startup screen
 (setq make-backup-files nil)            ;disable backups
 (setq-default c-basic-offset 4)         ;style variable
 (setq-default indent-tabs-mode nil)     ;spaces instead of tabs
@@ -13,3 +13,10 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+;; XEmacs version
+;(custom-set-variables
+; '(gutter-buffers-tab-visible-p nil)
+; '(menubar-visible-p nil t)
+; '(scrollbars-visible-p nil)
+; '(toolbar-visible-p nil))
