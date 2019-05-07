fa5809b7540cd0ea933f638df205f16ebd281191
s <s@debian>
2019-05-07 09:10:46 +0700

    openbox

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 NAME=$(realpath $0)
 DIR=$(dirname $NAME)
 find $DIR -path $DIR/.git -prune -o -path $NAME -prune -o -type f -print |
-while read line; do
-    target=$(echo $line | sed s!$DIR/!$HOME/.!)
-    install -Dm644 $line $target
-    ln -sf $line $target
-done
+    while read line; do
+        target=$(echo $line | sed s!$DIR/!$HOME/.!)
+        install -Dm644 $line $target
+        ln -sf $line $target
+    done
