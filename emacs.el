8e2a4f2dd69e330d74f0ad0e97d47ac777c936ea
concerned-guy <34766392+concerned-guy@users.noreply.github.com>
2019-05-29 12:49:12 +0700

    new emacs

--- emacs.el
+++ emacs.el
@@ -1,22 +1,13 @@
 ;;; emacs.el --- emacs initialization file
 
-;; Global behavior
+(package-initialize)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
 (setq auto-save-default nil)            ;disable autosaves
 (setq auto-save-list-file-prefix nil)   ;disable listing of autosaves
+(setq default-frame-alist '((vertical-scroll-bars) (tool-bar-lines . 0)))
 (setq inhibit-startup-message t)        ;disable startup screen
 (setq make-backup-files nil)            ;disable backups
 (setq-default c-basic-offset 4)         ;style variable
 (setq-default indent-tabs-mode nil)     ;spaces instead of tabs
-
-;; Minor modes
-(global-font-lock-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-
-;; XEmacs version
-;(custom-set-variables
-; '(gutter-buffers-tab-visible-p nil)
-; '(menubar-visible-p nil t)
-; '(scrollbars-visible-p nil)
-; '(toolbar-visible-p nil))
